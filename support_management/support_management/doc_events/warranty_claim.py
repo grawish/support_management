2628af274abb03060552b0104c4abd8fe68e5a47
Grawish Sachdeva <41504211+grawish@users.noreply.github.com>
2024-02-06 17:22:08 +0530

    fix: handle empty visits in warranty calculations
    Ensure that the warranty claim update logic handles cases where the
    visit list is empty. Previously, the calculation of man days and total
    expenses did not account for the potential absence of visits, which
    could lead to incorrect data in fields `custom_no_of_man_days` and
    `custom_total_expenses`. The update includes a check to only perform
    calculations when there are visits present, thus avoiding any unintended
    results for empty visit lists.

--- support_management/support_management/doc_events/warranty_claim.py
+++ support_management/support_management/doc_events/warranty_claim.py
@@ -2,8 +2,9 @@ import frappe
 
 def on_update(doc, method):
 #     set length of visits to no of days in warranty period
-    doc.custom_no_of_man_days = len(doc.custom_visits)
-    total_expenses = 0
-    for visit in doc.custom_visits:
-        total_expenses += visit.custom_total_expenses
-    doc.custom_total_expenses = total_expenses
+    if len(doc.custom_visits) > 0:
+        doc.custom_no_of_man_days = len(doc.custom_visits)
+        total_expenses = 0
+        for visit in doc.custom_visits:
+            total_expenses += visit.custom_total_expenses
+        doc.custom_total_expenses = total_expenses
