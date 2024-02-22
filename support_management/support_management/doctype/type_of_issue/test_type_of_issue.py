63d70770c9b007d3b0c1d6c3eaec24f59138db46
Ayan Dhara <git@ayandhara.me>
2024-02-22 20:14:41 +0530

    feat: added issue type

--- support_management/support_management/doctype/type_of_issue/test_type_of_issue.py
+++ support_management/support_management/doctype/type_of_issue/test_type_of_issue.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestTitleOfIssue(FrappeTestCase):
+class TestTypeOfIssue(FrappeTestCase):
 	pass
--- support_management/support_management/doctype/type_of_issue/type_of_issue.py
+++ support_management/support_management/doctype/type_of_issue/type_of_issue.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class TitleOfIssue(Document):
+class TypeOfIssue(Document):
 	pass
